68b17e0e6af4b3d810b909e12f2b74fb31673fd4
dluman <dluman@allegheny.edu>
2025-05-22 13:18:40 +0000

    Updating tag to reflect module import

--- build.zig
+++ build.zig
@@ -4,6 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const module = b.addModule("docker", .{
+        .root_source_file = b.path("src/direct.zig"),
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "docker", 
         .root_source_file = b.path("src/direct.zig"),
