1eed4789525051847241172b47a55bc503966fc2
dluman <dluman@allegheny.edu>
2025-05-28 14:46:26 +0000

    Adding static library build -- this isn't an application, after all

--- build.zig
+++ build.zig
@@ -1,16 +1,19 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const docker_module = b.addModule("docker", .{ .root_source_file = b.path("src/direct.zig"), .target = target, .optimize = optimize });
+    _ = b.addModule("docker", .{ .root_source_file = b.path("src/direct.zig"), .target = target, .optimize = optimize });
 
-    try b.modules.put(b.dupe("docker"), docker_module);
+    const lib = b.addStaticLibrary(.{ .name = "docker", .root_source_file = b.path("src/direct.zig"), .target = target, .optimize = optimize });
+
+    b.installArtifact(lib);
 
     const unit_tests = b.addTest(.{ .root_source_file = b.path("test.zig"), .target = target, .optimize = optimize });
 
     b.installArtifact(unit_tests);
+
     const unit_test = b.addRunArtifact(unit_tests);
     const unit_test_step = b.step("test", "Run unit tests.");
     unit_test_step.dependOn(&unit_test.step);
